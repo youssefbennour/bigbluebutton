2be3f5ac9c1d876a77ec54b5c39b7164ad67c179
youssef.bennour <youssefbennour@takiacademyteam.com>
2024-11-21 11:11:33 +0100

    FEAT: enable unmute even when user is locked

--- akka-bbb-apps/src/main/scala/org/bigbluebutton/core/apps/voice/VoiceHdlrHelpers.scala
+++ akka-bbb-apps/src/main/scala/org/bigbluebutton/core/apps/voice/VoiceHdlrHelpers.scala
@@ -5,7 +5,6 @@ import org.bigbluebutton.core.models.{ Users2x, VoiceUsers }
 import org.bigbluebutton.core.running.{ LiveMeeting }
 import org.bigbluebutton.LockSettingsUtil
 import org.bigbluebutton.SystemConfiguration
-import org.bigbluebutton.core.models.Roles
 
 object VoiceHdlrHelpers extends SystemConfiguration {
   def isGlobalAudioSubscribeAllowed(
@@ -38,11 +37,8 @@ object VoiceHdlrHelpers extends SystemConfiguration {
           liveMeeting.voiceUsers
         )
 
-        val hasUnmutePermission = user.role == Roles.MODERATOR_ROLE
-
         (applyPermissionCheck &&
           !isCallerBanned &&
-          hasUnmutePermission &&
           liveMeeting.props.meetingProp.intId == meetingId &&
           liveMeeting.props.voiceProp.voiceConf == voiceConf)
       }
